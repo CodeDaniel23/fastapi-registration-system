ec43047516fca6b50ba9bd644371d3eeb3d0585d
CodeDaniel23 <danielpauljalli@gmail.com>
2026-04-28 16:19:27 +0530

    Fix database connection string for Render PostgreSQL

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql+psycopg2://register_db_wmji_user:REAL_PASSWORD@dpg-xxxxx.oregon-postgres.render.com/register_db_wmji"
+DATABASE_URL = "postgresql+psycopg2://register_db_wmji_user:gBg22aeq3lyrH0yYdBFDVwk2DXc2yYlX@dpg-d7o7tsj7uimc73bmasv0-a.oregon-postgres.render.com/register_db_wmji"
 
 engine = create_engine(
     DATABASE_URL,
