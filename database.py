73a226fb4129cc1ec48ed1981efd0597ae3144c5
CodeDaniel23 <danielpauljalli@gmail.com>
2026-04-28 16:06:02 +0530

    Fixed Render database credentials

--- database.py
+++ database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql+psycopg2://register_db_wmji_user:password@dpg-xxxxx.oregon-postgres.render.com/register_db_wmji"
-
+DATABASE_URL = "postgresql+psycopg2://register_db_wmji_user:REAL_PASSWORD@dpg-xxxxx.oregon-postgres.render.com/register_db_wmji"
 
 engine = create_engine(
     DATABASE_URL,
