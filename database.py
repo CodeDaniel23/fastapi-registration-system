5d2579f7ac9861daa0f7b6de3d693428eec487c2
CodeDaniel23 <danielpauljalli@gmail.com>
2026-04-28 15:53:08 +0530

    Fixed Render PostgreSQL SSL connection

--- database.py
+++ database.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://register_db_wmji_user:gBg22aeq3lyrH0yYdBFDVwk2DXc2yYlX@dpg-d7o7tsj7uimc73bmasv0-a.oregon-postgres.render.com/register_db_wmji"
+DATABASE_URL = "postgresql+psycopg2://register_db_wmji_user:password@dpg-xxxxx.oregon-postgres.render.com/register_db_wmji"
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"})
 
 
 SessionLocal = sessionmaker(
