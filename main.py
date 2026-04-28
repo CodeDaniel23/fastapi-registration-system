ecb280618c805abe34f38ba3cb8eccd4be817b0f
CodeDaniel23 <danielpauljalli@gmail.com>
2026-04-28 17:35:57 +0530

    Fix Jinja2 cache issue on Render

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ models.Base.metadata.create_all(bind=engine)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+templates = Jinja2Templates(
+    directory=os.path.join(BASE_DIR, "templates")
+)
+
+templates.env.cache = {}
 
 app=FastAPI()
 
